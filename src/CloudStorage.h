26ddbb5566e40c8bc6e947ab7ecc5cf6565a1e0f
Gil Maimon <gilm2121@gmail.com>
2018-12-15 16:09:29 +0200

    added comments

--- src/CloudStorage.h
+++ src/CloudStorage.h
@@ -16,36 +16,43 @@ public:
     this->_password = pass;
   }
 
+  // Method for storing a key/value pair
   template <class Ty>
-  bool put(String key, Ty value) {    
-    String jsonString = getPutRequestJson(key, value);
+  bool put(String key, Ty value) {
+    // Build request json 
+    String jsonString = buildStoreObjectRequestJson(key, value);
 
+    // Construct http request
     RequestType request(
       _baseServerUrl + "/data/object", 
       http::Method::POST, 
       jsonString
     );
-    
     request.addHeader("Content-Type", "application/json");
+    
+    // Execute request and return success status
     http::Response response = request.execute();
     return response.statusCode == 200;
   }
 
+  // Method for retrieving key/value pair
   template <class Ty>
   Ty get(String key) {
-    // Construct Http Request
-    String jsonString = getGetRequestJson(key);
-
+    // Build request json 
+    String jsonString = buildGetObjectRequestJson(key);
+    
+    // Construct http request
     RequestType request(
       _baseServerUrl + "/data/object", 
       http::Method::GET, 
       jsonString
     );
-    
     request.addHeader("Content-Type", "application/json; charset=utf-8");
+    
+    //Execute request
     http::Response response = request.execute();
 
-    // Parse response and return value
+    // Parse response body and extract the wanted value
     StaticJsonBuffer<300> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(response.body);
     return getValueByKey<Ty>(root["result"], key);
@@ -55,40 +62,51 @@ private:
   String _username, _password;
   const String _baseServerUrl;
 
+  // Utility method for constructing *Store* request json string
   template <class Type>
-  String getPutRequestJson(String key, Type value) {
+  String buildStoreObjectRequestJson(String key, Type value) {
+    // Compose request json object
     StaticJsonBuffer<200> jsonBuffer;
-    
     JsonObject& root = jsonBuffer.createObject();
     root["username"] = _username;
     root["password"] = _password;
     root["key"] = key;
     root["value"] = value;
 
+    // Return string form of the object
     String jsonString;
     root.printTo(jsonString);
     return jsonString;
   }
 
-  String getGetRequestJson(String key) {
+  // Utility method for constructing *Get* request json string
+  String buildGetObjectRequestJson(String key) {
+    // Compose request json object
     StaticJsonBuffer<200> jsonBuffer;
-    
     JsonObject& root = jsonBuffer.createObject();
     root["username"] = _username;
     root["password"] = _password;
     root["key"] = key;
 
+    // Return string form of the object
     String jsonString;
     root.printTo(jsonString);
     return jsonString;
   }
 
+  // Method for accessing nested json objects with '.' seperated keys.
+  // for examples "name.first.english" for accessing name:{.., first:{ english: "MyName", .... }}
   template <class Type>
   Type getValueByKey(JsonObject& root, String key) {
+    // in case no '.' is in the key, return the corresponding value
     if(key.indexOf('.') == -1) return root[key];
+
+    // split key into parent and remainder
+    // example: "user.name.fullname" => parentKey("user"), remainder("name.fullname")
     String parentKey = key.substring(0, key.indexOf('.'));
     String remainder = key.substring(key.indexOf('.') + 1);
 
+    // traverse the parentKey object
     return getValueByKey<Type>(root[parentKey], remainder);
   } 
 };
--- src/WifiConnection.h
+++ src/WifiConnection.h
@@ -6,11 +6,13 @@
 namespace WifiConnection {  
   ESP8266WiFiMulti WiFiMulti;
   
+  // Sets wifi mode to STA and tries to connect to a network
   void tryConnect(String ssid, String pass) {
     WiFi.mode(WIFI_STA);
     WiFiMulti.addAP(ssid.c_str(), pass.c_str());
   }
   
+  // Method for checking if currently connected to wifi
   boolean isConnected() {
     return WiFiMulti.run() == WL_CONNECTED;
   }
