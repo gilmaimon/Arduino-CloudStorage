1223def3dfc3ff5d41bfba55e31735819e5ec6f6
Gil Maimon <gilm2121@gmail.com>
2018-12-22 01:19:02 +0200

    stopped using WifiMulti for connection to networks

--- src/WifiConnection.h
+++ src/WifiConnection.h
@@ -1,19 +1,16 @@
 #pragma once
 
 #include <WiFi.h>
-#include <WiFiMulti.h>
 
-namespace WifiConnection {  
-  WiFiMulti wifiMulti;
-  
+namespace WifiConnection {    
   // Sets wifi mode to STA and tries to connect to a network
   void tryConnect(String ssid, String pass) {
     WiFi.mode(WIFI_STA);
-    wifiMulti.addAP(ssid.c_str(), pass.c_str());
+    WiFi.begin(ssid.c_str(), pass.c_str());
   }
   
   // Method for checking if currently connected to wifi
   boolean isConnected() {
-    return wifiMulti.run() == WL_CONNECTED;
+    return WiFi.status() == WL_CONNECTED;
   }
 };
